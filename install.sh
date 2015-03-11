04068825e59b650ba638d9bc2a17b3edc24df066
zoombafu <apstinky@yahoo.com>
2015-03-11 18:27:36 -0500

    added installer script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+files=( .vimrc .zshrc .tmux.conf .xmobarrc .Xresources )
+copy_cmd="cp --recursive --interactive --backup --suffix=.backup"
+
+mkdir --parents ~/.xmonad
+
+echo "Copying over files..."
+
+$copy_cmd $PWD/.xmonad/xmonad.hs ~/.xmonad
+
+for file in ${files[@]}
+do
+    echo $file
+    $copy_cmd $PWD/$file ~/ 
+done
+
+echo "Done!"
