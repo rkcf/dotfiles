43d330d59d26e675ae8c1aab3290a9589d86b1a3
zoombafu <apstinky@yahoo.com>
2015-04-27 23:33:04 -0500

    worked on xmonad config, use termite now

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,27 +9,57 @@ import XMonad.Prompt.Man
 import XMonad.Prompt.Shell
 import XMonad.Actions.GridSelect
 import XMonad.Layout.Accordion
+import XMonad.Actions.SpawnOn
+import XMonad.Layout.Spacing
 import qualified Data.Map as M
 
 main = do
     xmproc <- spawnPipe "/bin/xmobar ~/.xmobarrc"
     xmonad $ defaultConfig
-        { keys = myKeys <+> keys defaultConfig
-        , manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-            { ppOutput = hPutStrLn xmproc
-            , ppTitle = xmobarColor "green" "" . shorten 50
-            }
-        , terminal      = "urxvt"
-        , borderWidth   = 0
-        , modMask       = mod4Mask
-        }
+        { XMonad.keys = myKeys <+> keys defaultConfig
+		, XMonad.workspaces = myWorkspaces
+        , XMonad.manageHook = manageSpawn <+> manageDocks <+> manageHook defaultConfig 
+		, XMonad.layoutHook = avoidStruts $ myLayout
+		, XMonad.startupHook = myStartupHook
+		, XMonad.logHook = dynamicLogWithPP xmobarPP
+			{ ppOutput = hPutStrLn xmproc
+			, ppTitle = xmobarColor "green" "" . shorten 50
+			}
+		, XMonad.terminal      = myTerminal
+        , XMonad.borderWidth   = myBorderWidth
+        , XMonad.modMask       = myModMask
+		}
+
+myModMask = mod4Mask
+myTerminal = "termite"
+myBorderWidth = 0
+
+
+myLayout = tiled ||| Mirror tiled ||| Full
+	 where
+		tiled = spacing 15 $ Tall nmaster delta ratio
+		nmaster = 1
+		ratio = 1/2
+		delta = 3/100
+
+myWorkspaces = 	
+	[ "irc"
+	, "term"
+	, "web"
+	, "steam"
+	, "5"
+	, "6"
+	, "7"
+	]
+
+myStartupHook :: X ()
+myStartupHook = do
+	spawnOn "irc" "startIrc"
+	spawnOn "web" "chromium"
+	spawnOn "term" myTerminal
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList
     [ ((modm, xK_F1), manPrompt defaultXPConfig)
     , ((modm, xK_F2), shellPrompt defaultXPConfig)
     , ((modm, xK_g), goToSelected defaultGSConfig)
     ]
-
-
--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-files=( .vimrc .zshrc .tmux.conf .xmobarrc .Xresources )
+files=( .vimrc .zshrc .tmux.conf .xmobarrc )
 copy_cmd="cp --recursive --interactive --backup --suffix=.backup"
 
-mkdir --parents ~/.xmonad
+mkdir --parents ~/.xmonad ~/.config/termite
 
 echo "Copying over files..."
 
 $copy_cmd $PWD/.xmonad/xmonad.hs ~/.xmonad
+$copy_cmd $PWD/.config/termite/config ~/.config/termite
 
 for file in ${files[@]}
 do
