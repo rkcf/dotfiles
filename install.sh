90fac0135d51d29ecf3886c7a4bf05275ce3757a
RKCF <steinkean@gmail.com>
2017-06-19 18:22:54 -0400

    remove termite config

--- install.sh
+++ install.sh
@@ -3,12 +3,11 @@
 files=( .vimrc .zshrc .tmux.conf .xmobarrc )
 copy_cmd="cp --recursive --interactive --backup --suffix=.backup"
 
-mkdir --parents ~/.xmonad ~/.config/termite
+mkdir --parents ~/.xmonad
 
 echo "Copying over files..."
 
 $copy_cmd $PWD/.xmonad/xmonad.hs ~/.xmonad
-$copy_cmd $PWD/.config/termite/config ~/.config/termite
 
 for file in ${files[@]}
 do
